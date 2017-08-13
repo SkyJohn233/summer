814bab05b24d1dbb41c14b9de3def509f39c88c6
Tizen Ye <yzs981130@126.com>
2017-08-13 18:58:03 +0800

    little bug fixed

--- summer/Dialogs/SummerDialog.cs
+++ summer/Dialogs/SummerDialog.cs
@@ -21,6 +21,14 @@ namespace summer.Dialogs
             return Task.CompletedTask;
         }
 
+        private String GetRandomResString()
+        {
+            String[] randomResponseString = { $"不是很懂你们大佬在说什么，请问我一点关于俱乐部的问题吧！", $"啊咧咧，我刚刚开小差了，请换种方式再说一下吧", $"主人还没有教我这些哦，请问我一些俱乐部的问题吧" };
+            Random ran = new Random();
+            int key = ran.Next(0, randomResponseString.Length - 1);
+            return randomResponseString[key];
+        }
+
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
             var activity = await result as Activity;
@@ -28,6 +36,8 @@ namespace summer.Dialogs
             var client = new HttpClient();
             var knowledgeBaseId = "bb5fa08b-3f70-4453-9da4-879e12657f84";
             var SubscriptionKey = "285d4c7ef5d4430895df6fb82b8219c8";
+            
+
             if (activity != null)
             {
                 var body = $"{{\"question\": \"{activity.Text}\"}}";
@@ -46,25 +56,34 @@ namespace summer.Dialogs
                     response = await client.PostAsync(uri, content);
                 
                 }
-
-                response.EnsureSuccessStatusCode();
-                string json = await response.Content.ReadAsStringAsync();
-
-                JObject resp = JObject.Parse(json);
-                IList<JToken> results = resp["answers"].Children().ToList();
-                IList<QnAMakerResult> searchResults = new List<QnAMakerResult>();
-                foreach (JToken re in results)
+                try
                 {
-                    QnAMakerResult searchResult = JsonConvert.DeserializeObject<QnAMakerResult>(re.ToString());
-                    searchResults.Add(searchResult);
-                }
+                    response.EnsureSuccessStatusCode();
+                    string json = await response.Content.ReadAsStringAsync();
 
-                var answer = searchResults[0].Answer;
-                if (answer.Equals("No good match found in the KB"))
-                    answer = "不是很懂你们大佬在说什么，请问我一点关于俱乐部的问题吧！";
-                if (searchResults[0].Score < 50.0)
-                    answer = "不好意思，我对我的回答不很自信，我觉得你可能在问：" + searchResults[0].Questions[0] + "我觉得可能的答案是：" + searchResults[0].Answer;
-                await context.PostAsync(answer);
+                    JObject resp = JObject.Parse(json);
+                    IList<JToken> results = resp["answers"].Children().ToList();
+                    IList<QnAMakerResult> searchResults = new List<QnAMakerResult>();
+                    foreach (JToken re in results)
+                    {
+                        QnAMakerResult searchResult = JsonConvert.DeserializeObject<QnAMakerResult>(re.ToString());
+                        searchResults.Add(searchResult);
+                    }
+
+                    var answer = searchResults[0].Answer;
+                    if (answer.Equals("No good match found in the KB"))
+                        answer = GetRandomResString();
+                    else if (searchResults[0].Score > 97.0)
+                        answer = "嘿嘿，就知道你要问我这个，你可算问对人了。" + searchResults[0].Answer;
+                    else if (searchResults[0].Score < 50.0)
+                        answer = "不好意思，我对我的回答不很自信，我觉得你可能在问：" + searchResults[0].Questions[0] + "我觉得可能的答案是：" + searchResults[0].Answer;
+                    await context.PostAsync(answer);
+                }
+                catch(HttpRequestException)
+                {
+                    await context.PostAsync("我好像连不上微软认知服务了。。。");
+                }
+                
             }
             context.Wait(MessageReceivedAsync);
         }
